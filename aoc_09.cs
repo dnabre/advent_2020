efe580e55f4fca68974f3e70f616960cce47e106
dnabre <alexander@happybunnylabs.com>
2020-12-09 12:20:13 -0700

    Day 9, Part 2 done

--- aoc_09.cs
+++ aoc_09.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Text;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Security.AccessControl;
+
 
 
 /*
 	Solutions found:
 	Part 1: 731031916
-	Part 2: 
+	Part 2: 93396727
 	
 */
 
@@ -25,13 +24,13 @@ namespace advent_2020
       
         public static void Run(string[] args)
         {
-           long part1_answer;
+            long part1_answer;
 		   
-		    Console.WriteLine("AoC Problem 09");
+            Console.WriteLine("AoC Problem 09");
             part1_answer = Part1(args);
             Console.Write("\n");
             //part1_answer = 127L;
-			Part2(part1_answer, args);
+            Part2(part1_answer, args);
         }
 
 
@@ -78,7 +77,7 @@ namespace advent_2020
                 HashSet<long> sums = getAllPartsLast5(nums, n - 25);
                 if (sums.Contains(nums[n]))
                 {
-                //    Console.WriteLine($"\t {nums[n]} is a sum");
+                    //    Console.WriteLine($"\t {nums[n]} is a sum");
                 }
                 else
                 {
@@ -87,14 +86,14 @@ namespace advent_2020
                   
                     
                     Console.WriteLine($"\n\tPart 1 Solution: {solution}");
-                  return solution;
+                    return solution;
                 }
             }
             
             
 
             Console.WriteLine($"\n\tPart 1 Solution: {solution}");
-			return solution;
+            return solution;
         }
 
     
@@ -103,134 +102,84 @@ namespace advent_2020
             Console.WriteLine("   Part 2");
             String[] lines = System.IO.File.ReadAllLines(Part2Input);
             Console.WriteLine("\tRead {0} inputs", lines.Length);
-			Console.Write($"\t Magic number from Part 1 is {target}");
+            Console.Write($"\t Magic number from Part 1 is {target}");
 
-			long[] numbers = new long[lines.Length];
-			for(int i=0; i < lines.Length; i++) {
-					numbers[i] = long.Parse(lines[i]);
-					if(numbers[i] == target) {
-						Console.WriteLine($" with index numbers[{i}]");
-					}
-					numbers[i] = target +1;
-			}
+            long[] numbers = new long[lines.Length];
+            for(int i=0; i < lines.Length; i++) {
+                numbers[i] = long.Parse(lines[i]);
+                if(numbers[i] == target) {
+                    Console.WriteLine($" with index numbers[{i}]");
+                }
+            }
+       
+           
+            
+            (int start, int length) = findSequence(target, numbers);
+            if ((start == 0) && (length == 0))
+            {
+                Console.WriteLine($"\t findSequence failed to find solution");
+            }
+            else
+            {
+                Console.WriteLine($"\t find returned {(start, length)}");
+            }
+
+            long min=numbers[start];
+            long max=numbers[start];
+           
+            for(int i=start; i < start+length; i++)
+            {
+                min=Math.Min(min,numbers[i]);
+                max=Math.Max(max,numbers[i]);
+            }
+
+            //Double check that range is the right sum
+            long test = naive_sum(numbers,start,length);
+       
 			
-			
+            if(test != target)  Console.WriteLine($"\t found solution: target={target} test of sum: {test}");
 
-			foreach(long l in sols) {
-				for(int i=0; i < numbers.Length; i++){
-					if(number[i] == l) {
-						Console.WriteLine($"\t{i} is {l}");
-						i=numbers.Length;
-					}
-				}
-			}
-
-			System.Environment.Exit(0)
-
-			int start; int length;
-			//(start,length) = findSequence(target, numbe	
-
-			
-			//(start,length) = bruteSequence(target, numbers);
-						
-			long min=numbers[start];
-			long max=numbers[start];
-			for(int i=start; i < start+length; i++) {
-				Console.WriteLine($"\tnumbers[{i}] = {numbers[i]}");
-				min=Math.Min(min,numbers[i]);
-				max=Math.Max(max,numbers[i]);
-
-			}
-
-			(int s, int e) = (start,start+length);
-			Console.WriteLine($"(start,length,end) = {(start,length, start+length-1)}")
-
-
-			long test;
-			test = naive_sum(numbers,start,length);
-
-			long test2 = 0;
-			
-
-			Console.WriteLine($"\t found solution: target={target} test of sum: {test}");
-
-			Console.WriteLine($"\t{(s,e)}\t length={length}\t{(numbers[start],numbers[start+length-1])}");
-			Console.WriteLine($"\t min = {min}, max = {max} sum = {min+max}");
-			// 2301111897 is too high
-			// 92538328 is too low
-			// answer is 93396727
+            Console.Write($"\t{(start, start+length-1)}\t length={length}\t{(numbers[start],numbers[start+length-1])}");
+            Console.WriteLine($"\t min = {min}, max = {max} sum = {min+max}");
+            // 2301111897 is too high
+            // 92538328 is too low
+            // answer is 93396727
           
 
-            Console.WriteLine($"\n\tPart 2 Solution: {0}");
+            Console.WriteLine($"\n\tPart 2 Solution: {min+max}");
         }
 
-		private static long naive_sum(long[] numbers, int start, int length) {
-			long result=0;
-			for(int i =0; i<length; i++ ){
-				result = numbers[start+i];
-			}
-			return result;
-		}
+        private static long naive_sum(long[] numbers, int start, int length) {
+            long result=0;
+            for(int i =0; i<length; i++ ){
+                result += numbers[start+i];
+            }
+            return result;
+        }
 
 		
-		private static (int start, int length) findSequence(long target, long[] numbers) {
-			int range_start, range_length;
-			long current_sum = 0L;
-			range_start=0;
-			do
-			{
+        private static (int start, int length) findSequence(long target, long[] numbers) {
+            int range_start, range_length;
+            long current_sum = 0L;
+            range_start=-1;
+            do
+            {
+                range_start++;
+                range_length = 2; 
+                current_sum = naive_sum(numbers, range_start,range_length);
+                while (current_sum < target)
+                {
+                    range_length++;
+                    current_sum +=numbers[range_start+range_length-1];
+                }
+                if(current_sum == target) {
+                    return (range_start,range_length-1);
+                }
+                range_start++;
+            } while ((range_start < numbers.Length-1) && (range_start+range_length < numbers.Length));
+            return (0, 0);
+        }
 
-				range_length = 2; 
-				current_sum = naive_sum(numbers, range_start,range_length);
-				while(current_sum < target) {
-			//		Console.WriteLine($"\tsum({range_start},{range_start+range_length})={current_sum}");
-
-					range_length++;
-					current_sum +=numbers[range_start+range_length-1];
-				}
-				if(current_sum == target) {
-					Console.WriteLine($"\tsum({range_start},{range_start+range_length})={current_sum} == {target}");
-					return (range_start,range_length);
-				}
-				range_start++;
-			} while ((range_start < numbers.Length-1) && (range_start+range_length < numbers.Length));
-			return (0, 0);
-		}
-		
-		private long[] sols = {30684790, 
-31929175, 
-32111531, 
-33239826, 
-34484211, 
-34933587, 
-35048416, 
-35733886, 
-37317716, 
-37982233, 
-51019481, 
-51993598, 
-52688226, 
-53564537, 
-53833200, 
-61755566, 
-62711937,}
-
-		private static (int start, int length) bruteSequence(long target, long[] numbers) {
-			int max_length = numbers.Length;
-			long sum=0;
-			for(int start=0; start < numbers.Length; start++) {
-				sum=0;
-				for(int length =2; (length < numbers.Length) && (start+length < numbers.Length) && (sum <target); length++) {
-					sum = naive_sum(numbers,start,length);
-					if(sum==target){
-						Console.WriteLine($"\tsum({start},{start+length}, {length})={sum} == {target}");
-						return (start,length);
-					}
-
-				}
-			}	
-			return (0,0);
-    	}
-	}
+   
+    }
 }
-
