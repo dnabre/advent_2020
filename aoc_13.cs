6b55f80a359de0272fc2e8d2bbdfac0b32654992
dnabre <alexander@happybunnylabs.com>
2020-12-13 12:01:24 -0700

    Day 13 Part 2 not working at all
    commit before complete do-over

--- aoc_13.cs
+++ aoc_13.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Text;
 using System.Collections.Generic;
-
+using System.Globalization;
 
 
 /*
@@ -21,14 +21,14 @@ namespace advent_2020
 
         public static void Run (string[] args) {
             Console.WriteLine ("AoC Problem 13");
-        //    Part1(args);
+            //Part1(args);
             Console.Write("\n");
-            Part2(args);
+           Part2(args);
         }
 
         private static void Part1(string[] args) {
             Console.WriteLine("   Part 1");
-            string[] lines =  System.IO.File.ReadAllLines(Part1Input);
+            string[] lines =  System.IO.File.ReadAllLines(Part2Input);
             Console.WriteLine("\tRead {0} inputs", lines.Length);
 			//Console.WriteLine($"\t{lines[0]}");
 			long target;
@@ -41,6 +41,7 @@ namespace advent_2020
 			foreach(String p in parts) {
 				if(p.Equals("x"))	continue;
 				long n = long.Parse(p);
+				
 				buses.Add(n);
 			}
 
@@ -67,41 +68,85 @@ namespace advent_2020
 
         private static void Part2(string[] args) {
             Console.WriteLine("   Part2");
-            string[] lines =  System.IO.File.ReadAllLines(TestInput1);
+            string[] lines =  System.IO.File.ReadAllLines(Part2Input);
             Console.WriteLine("\tRead {0} inputs", lines.Length);
-//Console.WriteLine($"\t{lines[0]}");
+			Console.WriteLine($"\t{lines[0]}");
 			long target;
+			long x_count = 0;
 			target = long.Parse(lines[0]);
-			//Console.WriteLine($"\t target = {target}");
+			target = 1068781;
+			Console.WriteLine($"\t target = {target}");
 			String inputs = lines[1];
-			//Console.WriteLine($"\t{lines[1]}");
+			Console.WriteLine($"\t{lines[1]}");
 			List<long> buses = new List<long>();
+			List<long> minute_after = new List<long>();
 			String[] parts = inputs.Split(',');
-			foreach(String p in parts) {
-				if(p.Equals("x"))	continue;
+			long timestamp_n = -1;
+			foreach(String p in parts)
+			{
+				timestamp_n++;
+				if(p.Equals("x"))
+				{
+					x_count++;
+					continue;
+					
+				}
 				long n = long.Parse(p);
 				buses.Add(n);
+				minute_after.Add(timestamp_n);
+				Console.WriteLine($"\t Bus {n} is at t+{timestamp_n} ");
 			}
 
-			Dictionary<long,long> bus_back = new Dictionary<long,long>();
-			List<long> next_stop= new List<long>(buses.Count);
-			long next = -1;
-			foreach(long bus in buses) {
-				next = GetNextStopAfter(bus,target);
-			//	Console.WriteLine($"\ttarget: {target} \tbus: {bus} \tnext_stop: {next}");
-				bus_back[next] = bus;
-				next_stop.Add(next);
-			}
-			long min_time = next;
-			foreach(long i in next_stop) {
-				min_time = Math.Min(i,min_time);
-			}
-			long next_bus = bus_back[min_time];
-			long minutes_until = min_time - target;
-			Console.WriteLine();
-			Console.WriteLine($"\t Next bus stop is {next_bus} at time {min_time}, {minutes_until} minutes in the future");
+			long[] bus_id = buses.ToArray();
+			long[] minutes = minute_after.ToArray();
+			long answer = -1;
 
-            Console.WriteLine($"\n\tPart 2 Solution: {0}");	
+			
+			long increment = 1;
+			
+			
+			
+			while (answer < 0)
+			{
+				int good_count = 0;
+				for (int i = 0; i < bus_id.Length; i++)
+				{
+					long r = WhichTime(bus_id[i], target);
+			//		Console.WriteLine($"\t{target} % {bus_id[i]} = {r}   {minutes[i]}"); 
+					if (r == minutes[i])
+					{
+						good_count++;
+									
+						
+					}
+					
+				}
+			//		Console.WriteLine();
+
+				
+				if (good_count == bus_id.Length)
+				{
+					answer = target;
+					break;
+				}
+				else
+				{
+					target += increment;
+					if (target <= 0)
+					{
+						Console.WriteLine($"Overflow on target = {target}");
+						System.Environment.Exit(0);
+					}
+				}
+				
+			}
+			
+			
+			
+			
+			
+			
+			Console.WriteLine($"\n\tPart 2 Solution: {target}");
         }
 
 		private static long GetNextStopAfter(long bus, long target) {
@@ -114,7 +159,34 @@ namespace advent_2020
 			return next;
 		}
 
-    }
-	
+		private static long WhichTime(long b, long target)
+		{
+			long m1,m2;
+			m1 = target % b;
+			m2 = Math.Abs(m1 - b);
+			return Math.Min(m1, m2);
 
+		}
+		
+		private static long GetBus(long timestamp, long minutes_after, long[] bus)
+		{
+			for (int i = 0; i < bus.Length; i++)
+			{
+				if (bus[i] == -1)
+				{
+					continue;
+				}
+
+				long m;
+				m = timestamp % bus[i];
+				if (minutes_after == m)
+				{
+					return bus[i];
+				}
+			}
+			return -1;
+		}
+		
+    }
+    
 }
--- aoc_util.cs
+++ aoc_util.cs
@@ -11,124 +11,180 @@ using System.Collections.Generic;
 
 namespace advent_2020
 {
-	static class Utility {
-			public static bool Array2DEqual<T>(T[,] a, T[,] b) {				
-				(int w, int h) size_a = (a.GetLength(0), a.GetLength(1));
-				(int w, int h) size_b = (b.GetLength(0), b.GetLength(1));
-				if(size_a.w != size_b.w) return false;
-				if(size_a.h != size_b.h) return false;
-				for(int y = 0; y < size_a.h; y++) {
-					for(int x=0; x < size_a.w; x++) {
-						if(!a[x,y].Equals(b[x,y])) return false;
-					}
-				}
+    static class Utility {
+        public static bool Array2DEqual<T>(T[,] a, T[,] b) {				
+            (int w, int h) size_a = (a.GetLength(0), a.GetLength(1));
+            (int w, int h) size_b = (b.GetLength(0), b.GetLength(1));
+            if(size_a.w != size_b.w) return false;
+            if(size_a.h != size_b.h) return false;
+            for(int y = 0; y < size_a.h; y++) {
+                for(int x=0; x < size_a.w; x++) {
+                    if(!a[x,y].Equals(b[x,y])) return false;
+                }
+            }
 
 
-				return true;
-			}
+            return true;
+        }
 
-			public static int[] StringArrayToIntArray(String[] lines)
-			{
-				int[] result = new int[lines.Length];
-				for (int i = 0; i < lines.Length; i++)
-				{
-					result[i] = int.Parse(lines[i]);
-				}
+        public static int[] StringArrayToIntArray(String[] lines)
+        {
+            int[] result = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                result[i] = int.Parse(lines[i]);
+            }
 
-				return result;
-			}
+            return result;
+        }
 
-			public static string[] AddToArray(string[] lines, string[] o)
-			{
-				String[] result = new String[lines.Length + o.Length];
-				for (int i = o.Length; i < lines.Length + o.Length; i++)
-				{
-					result[i] = lines[i - o.Length];
-				}
+        public static string[] AddToArray(string[] lines, string[] o)
+        {
+            String[] result = new String[lines.Length + o.Length];
+            for (int i = o.Length; i < lines.Length + o.Length; i++)
+            {
+                result[i] = lines[i - o.Length];
+            }
 
-				for (int i = 0; i < o.Length; i++)
-				{
-					result[i] = o[i];
-				}
+            for (int i = 0; i < o.Length; i++)
+            {
+                result[i] = o[i];
+            }
 
-				return result;
-			}
+            return result;
+        }
 
-			public static String MaxIntOfString(String[] inputs)
-			{
-				int max = int.Parse(inputs[0]);
-				foreach(String i in inputs)
-				{
-					max = Math.Max(max, int.Parse(i));
-				}
+        public static String MaxIntOfString(String[] inputs)
+        {
+            int max = int.Parse(inputs[0]);
+            foreach(String i in inputs)
+            {
+                max = Math.Max(max, int.Parse(i));
+            }
 
-				return max.ToString();
-			}
-			public static String MinIntOfString(String[] inputs)
-			{
-				int min = int.Parse(inputs[0]);
-				foreach(String i in inputs)
-				{
-					min = Math.Max(min, int.Parse(i));
-				}
+            return max.ToString();
+        }
+        public static String MinIntOfString(String[] inputs)
+        {
+            int min = int.Parse(inputs[0]);
+            foreach(String i in inputs)
+            {
+                min = Math.Max(min, int.Parse(i));
+            }
 
-				return min.ToString();
-			}
+            return min.ToString();
+        }
 
-			public static long IntPow(long e_base, long power)
-			{
-				if (power < 0)
-				{
-					throw new ArgumentException($"power {power} must be greater than 0");
-				}
+        public static long IntPow(long e_base, long power)
+        {
+            if (power < 0)
+            {
+                throw new ArgumentException($"power {power} must be greater than 0");
+            }
 
-				if (power == 0) return 1;
-				long a = 1;
-				while (power > 1)
-				{
-					if (power % 2 == 0)
-					{
-						e_base = e_base * e_base;
-						power = power / 2;
-					}
-					else
-					{
-						a = e_base * a;
-						e_base = e_base * e_base;
-						power = (power - 1) / 2;
-					}
-				}
+            if (power == 0) return 1;
+            long a = 1;
+            while (power > 1)
+            {
+                if (power % 2 == 0)
+                {
+                    e_base = e_base * e_base;
+                    power = power / 2;
+                }
+                else
+                {
+                    a = e_base * a;
+                    e_base = e_base * e_base;
+                    power = (power - 1) / 2;
+                }
+            }
 
-				return e_base * a;
-			}
+            return e_base * a;
+        }
 			
 			
-		public static void PrintMap(Char[,] map, bool tab=true) {
-				int width = map.GetLength(0);
-			int height = map.GetLength(1);
-			for(int y=0; y < height; y++) {
-				if(tab) Console.Write("\t");
-				for(int x=0; x < width; x++) {
-					Console.Write(map[x,y]);
+        public static void PrintMap(Char[,] map, bool tab=true) {
+            int width = map.GetLength(0);
+            int height = map.GetLength(1);
+            for(int y=0; y < height; y++) {
+                if(tab) Console.Write("\t");
+                for(int x=0; x < width; x++) {
+                    Console.Write(map[x,y]);
 					
-				}
-				Console.WriteLine();
-			}
-			return;
-		}
-		public static void PrintMap(String[] lines, bool tab=true) {
+                }
+                Console.WriteLine();
+            }
+            return;
+        }
+        public static void PrintMap(String[] lines, bool tab=true) {
 
-			int width = lines[0].Length;
-			int height = lines.Length;
-			for(int y=0; y < height; y++) {
-				if(tab) Console.Write("\t");
-				for(int x=0; x < width; x++) {
-					char c  = lines[y][x];
-					Console.Write(c);
-									}
-				Console.WriteLine();
-			}
-				return;
-		}
-	}
+            int width = lines[0].Length;
+            int height = lines.Length;
+            for(int y=0; y < height; y++) {
+                if(tab) Console.Write("\t");
+                for(int x=0; x < width; x++) {
+                    char c  = lines[y][x];
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+            }
+            return;
+        }
+
+        public static long ModuloInverse(long a, long m)
+        {
+            // result is module inverse of a with respect to m
+            long m0 = m;
+            long t, q;
+            long x0 = 0;
+            long x1 = 1;
+            if (m == 1)
+            {
+                return 0;
+            }
+            while (a > 1)
+            {
+                q = a / m;
+                t = m;
+                m = a % m;
+                a = t;
+                t = x0;
+                x0 = x1 - q * x0;
+                x1 = t;
+            }
+
+            if (x1 < 0)
+            {
+                x1 = x1 + m0;
+            }
+
+            return x1;
+        }
+
+        public static long ChineseRemainderTheorem(    long[] modules, long[] b_i)
+        {
+            long N = 1;
+            //foreach (long n in modules) prod *= n;
+            for (int i = 0; i < modules.Length; i++)
+            {
+                N = N * modules[i];
+                
+            }
+            long result = 0;
+            for (int i = 0; i < modules.Length; i++)
+            {
+                long N_i = N / modules[i];
+                result += b_i[i] * Utility.ModuloInverse(N_i, modules[i]) * N_i;
+            }
+
+            return (result % N);
+        }
+
+
+
+    }
+		
+		
+		
+
 }
