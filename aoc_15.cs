ea20fc6d4b6795d3942b429d22104884d35350dc
dnabre <alexander@happybunnylabs.com>
2020-12-15 16:25:44 -0700

    Day 15
    Bunch of cleaning up

--- aoc_15.cs
+++ aoc_15.cs
@@ -25,7 +25,7 @@ namespace advent_2020
         {
             Console.WriteLine("AoC Problem 15");
             var watch = System.Diagnostics.Stopwatch.StartNew();
-        //          Part1(args);
+             Part1(args);
             watch.Stop();
             long time_part_1 = watch.ElapsedMilliseconds;
             Console.Write("\n");
@@ -36,17 +36,15 @@ namespace advent_2020
             Console.WriteLine($"Execution time, Part 1: {time_part_1} ms\t Part 2: {time_part_2} ms");
         }
 
-
-        //private static long[] Turns;
-
-
-        private static HashSet<long> used_numbers;
-        private static bool first_time = false;
-
+        private static Dictionary<long, long> TurnLastSpoke;
+        private static bool first_time;
         private static long previous_turn_needed;
+        
+        private static long end_turn = 2020;
+        private static long end_turn2 = 30000000;
+        
         private static void Speak(long turn, long speak_number)
         {
-       //     Console.Write($"\t\t Speak(turn: {turn} speak_number: {speak_number}");
             if (TurnLastSpoke.ContainsKey(speak_number))
             {
                 previous_turn_needed = TurnLastSpoke[speak_number];
@@ -58,115 +56,51 @@ namespace advent_2020
                 TurnLastSpoke.Add(speak_number, turn);
                 first_time = true;
             }
-        //    Console.WriteLine($" setting first_time: {first_time}");
         }
 
         private static void Part1(string[] args)
         {
-            long last_spoke = -1;
             Console.WriteLine("   Part 1");
             string[] lines = System.IO.File.ReadAllLines(Part1Input);
             Console.WriteLine("\tRead {0} inputs", lines.Length);
-
-          //  Turns = new Dictionary<long, long>();
-          //Turns = new long[end_turn + 50];
-            used_numbers = new HashSet<long>();
+            Console.WriteLine("\tProcessing {0} turns", end_turn);
+            
+            TurnLastSpoke = new Dictionary<long, long>();
             String[] nums = lines[0].Split(',');
-
             long current_turn = 0;
-
+            first_time = true;
+            
             for (int i = 0; i < nums.Length; i++)
             {
-                String rd = "th";
                 current_turn++;
                 long n = int.Parse(nums[i]);
                 Speak(current_turn, n);
-
-
-                if ((i == 0) || (i == 1) || (i == 2))
-                {
-                    if (i == 0)
-                    {
-                        rd = "st";
-                    }
-                    else if (i == 1)
-                    {
-                        rd = "nd";
-                    }
-                    else
-                    {
-                        rd = "rd";
-                    }
-                }
-
-            //    Console.WriteLine(
-           //         $"\t Turn {current_turn.ToString().PadLeft(4, ' ')}: The {current_turn}{rd} number spoken is a starting number {Turns[current_turn]}");
             }
 
-            last_spoke = int.Parse(nums[nums.Length - 1]);
-
+            long last_spoke = int.Parse(nums[nums.Length - 1]);
             current_turn++;
+        
             while (current_turn <= end_turn)
             {
-                long when_last_spoke;
-                if (first_time)
-                {
-                    when_last_spoke = -1;
-                }
-                else
-                {
-                    when_last_spoke = MostRecentSpoke(current_turn - 1, last_spoke);
-                }
-
-                //Console.Write($"\tTurn {current_turn}, last spoke: {last_spoke}, it was most recently spoken on turn {when_last_spoke}");
+                
                 long number = -1;
                 if (first_time)
                 {
                     number = 0;
+                    first_time = false;
                 }
                 else
                 {
-                    number = current_turn - 1 - when_last_spoke;
+                    long when_last_spoke = TurnLastSpoke[last_spoke];
+                    number = when_last_spoke - previous_turn_needed;
                 }
-                //Console.WriteLine($"\n\t\t picking number {number}  current_turn: {current_turn}  when_last_spoke: {when_last_spoke}");
-
-                if ((current_turn <= 5) || (current_turn > 2018))
-                {
-                    Console.WriteLine($"\t Turn {current_turn.ToString().PadLeft(4, ' ')}  speak number: {number}");
-                }
-
                 last_spoke = number;
-                first_time = false;
                 Speak(current_turn, number);
                 current_turn++;
             }
-
-
             Console.WriteLine($"\n\tPart 1 Solution: {last_spoke}");
         }
-
-        private static long end_turn = 2020;
-        private static long end_turn2 = 30000000;
-
-        private static long MostRecentSpoke(long current_turn, long last_spoke)
-        {
-            //     Console.WriteLine($"looking for last_spoke={last_spoke} with current_turn={current_turn}, first={first_time}, have used {used_numbers.Count}");
-            for (long j = current_turn - 1; j > 0; j--)
-            {
-                /*
-                if (Turns[j] == last_spoke)
-                {
-                    return j;
-                }*/
-            }
-
-            Console.WriteLine(
-                $"Error: Couldn't find last time spoke {last_spoke} looking back from turn {current_turn}");
-            return -1;
-        }
-
-        private static long when_last_spoke = -255;
-
+        
         private static void Part2(string[] args)
         {
             
@@ -174,17 +108,13 @@ namespace advent_2020
             string[] lines = System.IO.File.ReadAllLines(Part1Input);
             Console.WriteLine("\tRead {0} inputs", lines.Length);
             Console.WriteLine("\tProcessing {0} turns", end_turn2);
+            
             TurnLastSpoke = new Dictionary<long, long>();
             
-            used_numbers = new HashSet<long>();
             String[] nums = lines[0].Split(',');
-            
             long current_turn = 0;
             long last_spoke = int.Parse(nums[0]);
-            
-            when_last_spoke = 0;
             first_time = true;
-            
             
             for (int i = 0; i < nums.Length; i++)
             {
@@ -195,7 +125,6 @@ namespace advent_2020
 
             last_spoke = int.Parse(nums[nums.Length - 1]);
             current_turn++;
-        
             
             while (current_turn <= end_turn2)
             {
@@ -204,7 +133,6 @@ namespace advent_2020
                 if (first_time)
                 {
                     number = 0;
-                   
                     first_time = false;
                 }
                 else
@@ -213,18 +141,14 @@ namespace advent_2020
                     number = when_last_spoke - previous_turn_needed;
                     
                 }
-                
                 last_spoke = number;
                 Speak(current_turn, number);
                 current_turn++;
-             
             }
-
-
             Console.WriteLine($"\n\tPart 2 Solution: {last_spoke}");
         }
 
-        private static Dictionary<long, long> TurnLastSpoke;
+        
     }
     
 }
