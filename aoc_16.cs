867ff7df94c996ae2c49d24b7092246aa66052bb
dnabre <alexander@happybunnylabs.com>
2020-12-17 09:43:49 -0700

    17 template

--- aoc_16.cs
+++ aoc_16.cs
@@ -30,7 +30,7 @@ namespace advent_2020
             long time_part_1 = watch.ElapsedMilliseconds;
             Console.Write("\n");
             watch = System.Diagnostics.Stopwatch.StartNew();
-            Part2();
+    //        Part2();
             watch.Stop();
             long time_part_2 = watch.ElapsedMilliseconds;
             Console.WriteLine($"Execution time, Part 1: {time_part_1} ms\t Part 2: {time_part_2} ms");
@@ -40,9 +40,60 @@ namespace advent_2020
         private static void Part1()
         {
             Console.WriteLine("   Part 1");
-            string[] lines = System.IO.File.ReadAllLines(Part1Input);
+            string[] lines = System.IO.File.ReadAllLines(TestInput1);
             Console.WriteLine("\tRead {0} inputs", lines.Length);
-          
+
+            List<String> rule_strings = new List<string>();
+            List<String> number_strings = new List<string>();
+            bool rules_part = true;
+            bool my_ticket_part = false;
+            bool numbers_part = false;
+            List<String> my_ticket_strings = new List<string>();
+
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Equals(""))
+                {
+                    if (rules_part)
+                    {
+                        rules_part = false;
+                        my_ticket_part = true;
+
+                    }
+                    else if (my_ticket_part)
+                    {
+                        numbers_part = true;
+                        my_ticket_part = false;
+
+                    }
+                    
+                }
+                else
+                {
+                    if (rules_part)
+                    {
+                        rule_strings.Add(lines[i]);
+                    }
+                    else if (my_ticket_part)
+                    {
+                        my_ticket_strings.Add(lines[i]);
+                    }
+                    else if (numbers_part)
+                    {
+                        number_strings.Add(lines[i]);
+                    }
+                    
+                }
+            }
+
+            
+            
+            
+            
+            
+            
+            
             Console.WriteLine($"\n\tPart 1 Solution: {0}");
         }
         
@@ -56,6 +107,13 @@ namespace advent_2020
 		Console.WriteLine($"\n\tPart 2 Solution: {0}");
         }
 
+        private static void PrintList(List<String> lines)
+        {
+            foreach (String ln in lines)
+            {
+                Console.WriteLine($"\t {ln}");
+            }
+        }
         
     }
     
