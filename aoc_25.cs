b06386e0017dbcd23ddba62eab8780ba73512b71
dnabre <alexander@happybunnylabs.com>
2020-12-25 04:34:39 -0700

    template 24 & 25

---
+++ aoc_25.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+
+/*
+	Solutions found:
+	Part 1:
+	Part 2: 
+	
+*/
+
+namespace advent_2020 {
+  static class AOC_25 {
+    private const string Part1Input = "aoc_25_input_1.txt";
+    private const string Part2Input = "aoc_25_input_2.txt";
+    private const string TestInput1 = "aoc_25_test_1.txt";
+    private const string TestInput2 = "aoc_25_test_2.txt";
+    
+    public static void Run(string[] args) {
+      Console.WriteLine("AoC Problem 25");
+      var watch = Stopwatch.StartNew();
+      Part1();
+      watch.Stop();
+      long time_part_1 = watch.ElapsedMilliseconds;
+      Console.Write("\n");
+      watch = Stopwatch.StartNew();
+      Part2();
+      watch.Stop();
+      long time_part_2 = watch.ElapsedMilliseconds;
+      Console.WriteLine($"Execution time, Part 1: {time_part_1} ms\t Part 2: {time_part_2} ms");
+    }
+
+    
+    
+    private static void Part1() {
+      Console.WriteLine($"   Part 1");
+      string[] lines = File.ReadAllLines(TestInput1);
+      Console.WriteLine($"\tRead {0} inputs", lines.Length);
+  
+      Console.WriteLine($"\n\tPart 1 Solution: {0}");
+    }
+
+    private static void Part2() {
+      Console.WriteLine("   Part 2");
+      string[] lines = File.ReadAllLines(Part1Input);
+      Console.WriteLine("$\tRead {0} inputs", lines.Length);
+
+      Console.WriteLine($"\n\tPart 2 Solution: {0}");
+    }
+    
+    
+  }
+}
