2f73593f746ba882acf12abf441ad24df8f11230
dnabre <alexander@happybunnylabs.com>
2020-12-27 18:36:57 -0700

    Day 23. Part 1 rewritten using raw linked list

--- aoc_23.cs
+++ aoc_23.cs
@@ -1,8 +1,10 @@
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 /*
@@ -14,6 +16,45 @@ using System.Text;
 
 namespace advent_2020
 {
+    public class ListNode : IEquatable<ListNode>
+    {
+        public bool Equals(ListNode other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return val == other.val && Equals(next, other.next);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ListNode) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (val * 397) ^ (next != null ? next.GetHashCode() : 0);
+            }
+        }
+
+        public static bool operator ==(ListNode left, ListNode right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ListNode left, ListNode right)
+        {
+            return !Equals(left, right);
+        }
+
+        public int val;
+        public ListNode next;
+    }
+    
     internal static class AOC_23
     {
         private const string Part1Input = "aoc_23_input_1.txt";
@@ -26,12 +67,12 @@ namespace advent_2020
         {
             Console.WriteLine("AoC Problem 23");
             var watch = Stopwatch.StartNew();
-           // Part1();
+           Part1();
             watch.Stop();
             var time_part_1 = watch.ElapsedMilliseconds;
             Console.Write("\n");
             watch = Stopwatch.StartNew();
-                Part2();
+           //     Part2();
             watch.Stop();
             var time_part_2 = watch.ElapsedMilliseconds;
             Console.WriteLine($"Execution time, Part 1: {time_part_1} ms\t Part 2: {time_part_2} ms");
@@ -41,11 +82,11 @@ namespace advent_2020
         private static int turn;
         private static int[] pickup;
         private static int dest;
-
+        private static ListNode Head;
         private static void Part1()
         {
             Console.WriteLine("   Part 1");
-            var lines = File.ReadAllLines(Part1Input);
+            var lines = File.ReadAllLines(TestInput1);
             Console.WriteLine($"\tRead {lines.Length} inputs");
             Console.WriteLine($"\tRead {lines[0].Length} characters");
             Console.WriteLine($"\tRead: {lines[0]}");
@@ -53,218 +94,70 @@ namespace advent_2020
             char[] input_chars = lines[0].ToCharArray();
             int[] input_numbers = input_chars.Select(c => (int) Char.GetNumericValue(c)).ToArray();
             turn = 1;
-            current_cup = 0;
+            ListNode current_node;
+            ListNode dest_node;
             pickup = new int[3];
             dest = -1;
 
-            Stack<int> right = new Stack<int>();
-            Stack<int> left = new Stack<int>();
-            foreach (int i in input_numbers)
+         Head = new ListNode();
+            Head.val = input_numbers[0];
+            Head.next = null;
+
+
+            ListNode c_node = Head;
+            for (int i = 1; i < input_numbers.Length; i++)
             {
-                left.Push(i);
+                InsertAfter(c_node, input_numbers[i]);
+                c_node = c_node.next;
             }
 
-            while (left.Count > 0)
-            {
-                right.Push(left.Pop());
-            }
+            c_node.next = Head;           
+            
+            
+            Console.WriteLine($"\t Initial List: {ListNodeToString(Head)}");
+            
 
-            int temp;
-            current_cup = right.Peek();
-
-
-
-
-
-
-
+            current_node = Head;
+            
             int last_turn = 100;
 
-            int[] l_array;
+         
             while (turn <= last_turn)
             {
-                //bool printing_current = false;
-                Console.WriteLine("\n\n");
-                Console.WriteLine($"\t -- move {turn} --");
-                Console.Write($"\t cups: ");
+             
+               
+        
+                
+               
+                c_node = current_node.next;
+                pickup[0] = c_node.val;
+                c_node = c_node.next;
+                pickup[1] = c_node.val;
+                c_node = c_node.next;
+                pickup[2] = c_node.val;
+                current_node.next = c_node.next;
+               
 
-                l_array = left.ToArray();
-                for (int j = l_array.Length - 1; j >= 0; j--)
-                {
-                    int i = l_array[j];
-                    if (i == current_cup)
-                    {
-                        Console.Write($" ({i}) ");
-
-                    }
-                    else
-                    {
-                        Console.Write($" {i} ");
-                    }
-
-                }
-
-                foreach (int i in right)
-                {
-                    if (i == current_cup)
-                    {
-                        Console.Write($" ({i}) ");
-
-                    }
-                    else
-                    {
-                        Console.Write($" {i} ");
-                    }
-
-                }
-
-                Console.WriteLine();
-                PrintBothStacks(left, right, current_cup);
-                Console.WriteLine($"\t\t shift current ({current_cup}) to left");
-                left.Push(right.Pop());
-                PrintBothStacks(left, right, current_cup);
-                Console.WriteLine($"\t\t pick up 3 ");
-                if (right.Count >= 3)
-                {
-                    pickup[0] = right.Pop();
-                    pickup[1] = right.Pop();
-                    pickup[2] = right.Pop();
-                }
-                else
-                {
-                    // things get messy. 
-                    Console.WriteLine($"\t  3 picks on the back turn around, messinesss");
-
-                    Stack<int> pick = new Stack<int>();
-                    int picked = 0;
-                    while (right.Count > 0)
-                    {
-                        pick.Push(right.Pop());
-                        picked++;
-
-                    }
-                    //Console.WriteLine($"\t we were able to get {picked} from right -> pick {Utility.StackToStringLine(pick)}");
-
-                    // picked \in {0,1,2} 
-
-                    int[] left_array = left.ToArray();
-                    // Console.WriteLine($"\t left to array: {Utility.ArrayToStringLine(left_array)}  top: {left.Peek()} length = {left_array.Length}");
-
-
-                    int l_index = left_array.Length;
-                    //Console.WriteLine($"\t l_index = {l_index}, l_array.Length = {left_array.Length}");
-                    l_index--;
-                    //Console.WriteLine($"\t l_index = {l_index}, l_array.Length = {left_array.Length}");
-                    //Console.WriteLine($"\t left_array index = {l_index} or arr[{l_index}] = {left_array[l_index]}, pick = {Utility.StackToStringLine(pick)}, pick top: {pick.Peek()}");
-                    for (int i = 3 - picked; i > 0; i--)
-                    {
-                        //Console.WriteLine($"\t to pick up {i}");
-                        pick.Push(left_array[l_index]);
-
-                        //  Console.WriteLine($"\t left_array index = {l_index} or arr[{left_array[l_index]}], pick = {Utility.StackToStringLine(pick)}, pick top: {pick.Peek()}");
-                        l_index--;
-                    }
-
-                    l_index++;
-                    left = new Stack<int>();
-                    for (int i = l_index - 1; i >= 0; i--)
-                    {
-                        left.Push(left_array[i]);
-                    }
-                    //Console.WriteLine($"\t l_index = {l_index} left_array.Length = {left_array.Length} target: { l_index}   {Utility.ArrayToStringLine(left_array)}");
-
-                    ///Console.WriteLine(  $"\t l_index = {l_index} left_array.Length = {left_array.Length}   {Utility.ArrayToStringLine(left_array)}");
-
-
-                    // Console.WriteLine($"\t left: {Utility.StackToStringLine(left)}, top: {left.Peek()}");
-                    //PrintBothStacks(left,right,current_cup);
-
-                    pickup[2] = pick.Pop();
-                    pickup[1] = pick.Pop();
-                    pickup[0] = pick.Pop();
-
-
-
-                }
-
-
-
-
-                Console.WriteLine($"\t\t dest: {dest}");
-                dest = current_cup - 1;
+                dest = current_node.val - 1;
                 if (dest == 0) dest = 9;
-                Console.WriteLine($"\t\t dest: {dest}");
+              
                 while ((dest == pickup[0]) || (dest == pickup[1]) || (dest == pickup[2]))
                 {
                     dest--;
                     if (dest == 0) dest = 9;
-                    Console.WriteLine($"\t\t dest: {dest}");
+                 
                 }
 
-                Console.WriteLine($"\t\t dest: {dest}");
-                Console.WriteLine($"\t pick up: {pickup[0]}, {pickup[1]}, {pickup[2]} ");
-                Console.WriteLine($"\t destination: {dest}");
-                PrintBothStacks(left, right, current_cup);
-                Console.WriteLine($"\t\t moving everything to the right stack");
-                while (left.Count > 0)
+                dest_node = current_node;
+                while (dest_node.val != dest)
                 {
-                    temp = left.Pop();
-                    right.Push(temp);
+                    dest_node = dest_node.next;
                 }
-
-                PrintBothStacks(left, right, current_cup);
-                while (right.Peek() != dest)
-                {
-                    temp = right.Pop();
-                    left.Push(temp);
-                }
-
-                Console.WriteLine($"\t\t found dest({dest}) on the top of right:");
-                PrintBothStacks(left, right, current_cup);
-                left.Push(right.Pop());
-                Console.WriteLine($"\t\t move dest from right to left");
-                PrintBothStacks(left, right, current_cup);
-                left.Push(pickup[0]);
-                left.Push(pickup[1]);
-                left.Push(pickup[2]);
-                Console.WriteLine($"\t\t push pickups ({pickup[0]},{pickup[1]}, {pickup[2]}) to left");
-                PrintBothStacks(left, right, current_cup);
-                while (right.Count > 0)
-                {
-                    left.Push(right.Pop());
-                }
-
-                Console.WriteLine($"\t\t move everything to the left");
-                PrintBothStacks(left, right, current_cup);
-                right.Push(left.Pop());
-                Console.WriteLine($"\t\t Search for current_cup :{current_cup} on the top of right");
-                while ((right.Peek() != current_cup) && (left.Count > 0))
-                {
-                    right.Push(left.Pop());
-                }
-
-                PrintBothStacks(left, right, current_cup);
-                Console.WriteLine($"\t\t move current_cop ({current_cup}) to left, so new current is to the right");
-                if (right.Count == 0)
-                {
-                    left.Push(PopBottomOfStack(left));
-                }
-                else
-                {
-                    left.Push(right.Pop());
-                }
-
-                if (right.Count == 0)
-                {
-                    right.Push(PopBottomOfStack(left));
-                }
-
-                current_cup = right.Peek();
-                PrintBothStacks(left, right, current_cup);
-                Console.WriteLine(
-                    $"\t\t should be ready for next turn with current_cup ({current_cup}) on top of the right");
-
-
+              
+                InsertAfter(dest_node, pickup[2]);
+                InsertAfter(dest_node, pickup[1]);
+                InsertAfter(dest_node, pickup[0]);
+                current_node = current_node.next;
                 turn++;
 
             }
@@ -273,38 +166,25 @@ namespace advent_2020
             Console.WriteLine($"\t -- final --");
             Console.Write($"\t cups: ");
 
-            l_array = left.ToArray();
-            for (int j = l_array.Length - 1; j >= 0; j--)
+          
+            c_node = current_node;
+            do
             {
-                int i = l_array[j];
-                if (i == current_cup)
+                if (c_node == current_node)
                 {
-                    Console.Write($" ({i}) ");
-
+                    Console.Write($" ({c_node.val}) ");
                 }
                 else
                 {
-                    Console.Write($" {i} ");
+                    Console.Write($" {c_node.val} ");
                 }
+                c_node = c_node.next;
+                
+            } while (c_node.next != current_node);
+            
 
-            }
-
-            foreach (int i in right)
-            {
-                if (i == current_cup)
-                {
-                    Console.Write($" ({i}) ");
-
-                }
-                else
-                {
-                    Console.Write($" {i} ");
-                }
-
-            }
-        Console.WriteLine("\n\n\n");
             String final;
-            final = CupsAfterOne(left, right);
+            final = LLFinalPart1(current_node);
             
 
             Console.WriteLine();
@@ -314,8 +194,8 @@ namespace advent_2020
         private static void Part2()
         {
             Console.WriteLine("   Part 2");
-            var lines = File.ReadAllLines(Part1Input);
-            Console.WriteLine("$\tRead {0} inputs", lines.Length);
+            var lines = File.ReadAllLines(Part2Input);
+            Console.WriteLine($"\tRead {0} inputs", lines.Length);
 
             Console.WriteLine($"\tRead: {lines[0]}");
 
@@ -333,6 +213,13 @@ namespace advent_2020
                 left.Push(i);
             }
 
+            
+            for (int i = 10; i <= 1000000; i++)
+            {
+                left.Push(i);
+            }
+            
+            
             while (left.Count > 0)
             {
                 right.Push(left.Pop());
@@ -341,12 +228,15 @@ namespace advent_2020
             int temp;
             current_cup = right.Peek();
             
-            int last_turn = 100;
+            int last_turn = 10000000;
 
             int[] l_array;
             while (turn <= last_turn)
             {
-        
+                if (turn % 10000==0)
+                {
+                    Console.WriteLine($"\tTurn #{turn} \t current_cup={current_cup}");
+                }
 
                 l_array = left.ToArray();
                 for (int j = l_array.Length - 1; j >= 0; j--)
@@ -469,18 +359,65 @@ namespace advent_2020
 
             }
 
-        
+            long final;
+            int one=-1;
+                int two=-1;
+           // final = CupsAfterOne(left, right);
+           if (left.Contains(1))
+           {
+               
+               
+               if (left.Count < 10)
+               {
+                   Console.WriteLine(Utility.StackToStringLine(left));
+               }
+               else
+               {
+                   while (left.Peek() != 1)
+                   {
+                       two = left.Pop();
+                       one = left.Pop();
+                   }
+               }
 
-     
-            String final;
-            final = CupsAfterOne(left, right);
-            
-            
-            
-            
+
+           }
+           else
+           {// right contain 1
+               if (right.Count == 1)
+               {
+                   Console.WriteLine((Utility.StackToStringLine(right)));
+                   one = PopBottomOfStack(left);
+                   two = PopBottomOfStack(left);
+               }else  
+               {
+                   
+               
+                    
+                   while (right.Peek() != 1)
+                   {
+                       right.Pop();
+                   }
+
+                   if (right.Count < 10)
+                   {
+                       Console.WriteLine(Utility.StackToStringLine(right));
+                   }
+                   right.Pop(); // throw away one
+                   two = right.Pop();
+                   one = right.Pop();
+               }
+           }
+
+
+           
+           
+           final = one * two;
             
             
             Console.WriteLine($"\n\tPart 2 Solution: {final}");
+      
+            
         }
 
         private static void PrintBothStacks(Stack<int> left, Stack<int> right, int current_cup)
@@ -595,5 +532,77 @@ namespace advent_2020
 
             return sb.ToString();
         }
+        private static void InsertAfter(ListNode n, int i){
+
+            ListNode new_node = new ListNode {val = i, next = n.next};
+            n.next = new_node;
+        }
+
+        private static int DeleteAfter(ListNode n)
+        {
+            // n.next -> (delete.next->) p.next
+            ListNode after = n.next;
+            int result = after.val;
+            n.next = after.next;
+            return result;
+        }
+
+
+        private static String ListNodeToString(ListNode n)
+        {
+            if (n.next == null) return "[]";
+            ListNode start = n;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[ ");
+            do
+            {
+                sb.Append($"{n.val}, ");
+                n = n.next;
+            } while  (n!= start);
+
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append("]");
+            return sb.ToString();
+        }
+
+
+        private static void LLPrintState(int[] pickup, ListNode c_list, ListNode current_node, int dest)
+        {
+            /*
+           
+
+            Console.WriteLine(
+                $"\t\t {ListNodeToString(c_list)} \t pickups[{a},{b},{c}] \t current_node = {current_node.val} dest = {dest}");
+            */
+            int a, b, c;
+            a = pickup[0];
+            b = pickup[1];
+            c = pickup[2];
+            Console.WriteLine($"\t\t  pickups[{a},{b},{c}] \t current_node = {current_node.val} dest = {dest}");
+        }
+
+        private static String LLFinalPart1(ListNode current_node)
+        {
+            while (current_node.val != 1)
+            {
+                current_node = current_node.next;
+            }
+
+            ListNode one_node = current_node;
+
+            current_node = one_node.next;
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                sb.Append(current_node.val);
+                current_node = current_node.next;
+
+            } while (current_node != one_node);
+
+            return sb.ToString();
+        }
     }
+
+
+
 }
