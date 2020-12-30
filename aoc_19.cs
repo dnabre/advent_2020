8ec5773be589d98cb824ecd26d8d8086154d697f
dnabre <alexander@happybunnylabs.com>
2020-12-30 12:46:08 -0700

    day 19 part2 bad attempt at actually parsing

--- aoc_19.cs
+++ aoc_19.cs
@@ -141,36 +141,165 @@ namespace advent_2020
             Console.WriteLine($"\t Starting Rule:\n\t\t\t {start_rule}");
             rule_strings.Remove(start_rule);
 
-            String term_a="", term_b="";
-            foreach(String r in rule_strings) {
-                if(r.Contains("a")) { term_a = r;}
-                if(r.Contains("b")) { term_b = r;}
+            int term_a = -1;
+            int term_b = -1;
+            String term_a_s = "";
+            String term_b_s = "";
+            foreach (String r in rule_strings)
+            {
+                String[] parts = r.Split(':');
+                if (r.Contains("a"))
+                {
+                    term_a_s = r;
+                    term_a = int.Parse(parts[0]);
+                }
+
+                if (r.Contains("b"))
+                {
+                    term_b_s = r;
+                    term_b = int.Parse(parts[0]);
+                }
             }
-            rule_strings.Remove(term_a);
-            rule_strings.Remove(term_b);
+
+            rule_strings.Remove(term_a_s);
+            rule_strings.Remove(term_b_s);
 			
             Console.WriteLine($"\t Terminals:");
-            Console.WriteLine($"\t\t\t {term_a}");
-            Console.WriteLine($"\t\t\t {term_b}");
+            Console.WriteLine($"\t\t\t {term_a} :: {term_a_s}");
+            Console.WriteLine($"\t\t\t {term_b} :: {term_b_s}");
 
             
            
 
+            RuleLookup = new Dictionary<int, string>(rule_strings.Count);
+            foreach (String r in rule_strings)
+            {
+                String[] Parts = r.Split(':');
+                int r_num = int.Parse(Parts[0]);
+                String rule_string = Parts[1];
 
-            
-            
-            //	bool match = CYK(rule_strings, start_rule, term_a,term_b, message_strings[0]);
+                RuleLookup[r_num] = rule_string;
+                //    Console.WriteLine($"\t Storing Rule#: {r_num} \t {rule_string}");
+            }
 
-		
-            Console.WriteLine();
-			
-		            
+            foreach (String m_input in message_strings)
+            {
+
+                Console.WriteLine($"\n\t {message_strings[0]}");
+                bool match = CYK(rule_strings, start_rule, term_a, term_b, message_strings[0]);
+
+
+                Console.WriteLine();
+
+
+                Console.WriteLine($"\n\t {message_strings[0]} \t match: {match} ");
+                System.Environment.Exit(0);
+            }
+
             Console.WriteLine($"\n\tPart 2 Solution: {0}");
         }
-        
-        private static bool CYK(List<String> rule_strings, String start_rule, String term_a, String term_b, String test_string) {
+
+
+        private static Dictionary<int, String> RuleLookup;
+
+        private static bool CYK(List<String> rule_strings, String start_rule, int a_term, int b_term,
+            String test_string)
+        {
+
+            int n = test_string.Length;
+            int number_rules = rule_strings.Count;
+            Console.WriteLine($"\t Checking string {test_string} of length {$"n"}");
+            Console.WriteLine($"\t number of rules: {number_rules}");
+
+            LinkedList<int> terms = new LinkedList<int>();
+            foreach (char c in test_string)
+            {
+                if (c == 'a') terms.AddLast(a_term);
+                else if (c == 'b') terms.AddLast(b_term);
+                else
+                {
+                    Console.WriteLine($"\t parse failed, expect 'a' or 'b', saw '{c}' in {test_string}");
+                    return false;
+                }
+            }
+
+            int previous_terms;
+            while (terms.Count > 1)
+            {
+                previous_terms = terms.Count;
+                LinkedList<int> new_list = new LinkedList<int>();
+                Console.WriteLine($"\t Processing Terms: {Utility.LinkdedListToStringLine(terms)}");
+                
+                
+                while (terms.Count > 0)
+                {
+                    int left = terms.First.Value;
+                    terms.RemoveFirst();
+                    int right = terms.First.Value;
+                    terms.RemoveFirst();
+                    String rule_string = $"{left} {right}";
+                    int rule_number_match = -1;
+                    foreach (int r_num in RuleLookup.Keys)
+                    {
+                        String p_rule = RuleLookup[r_num];
+                        if (p_rule.Contains('|'))
+                        {
+                            String[] pparts = p_rule.Split('|');
+                            String r_first = pparts[0].Trim();
+                            String r_second = pparts[1].Trim();
+                            if (rule_string.Equals(r_first))
+                            {
+                                Console.WriteLine(
+                                    $"\t\t l_exact match '{rule_string}' to '{r_first}' in \"{p_rule}\", rule number {r_num}");
+
+                                rule_number_match = r_num;
+                                break;
+                            }
+                            if (rule_string.Equals(r_second))
+                            {
+                                Console.WriteLine(
+                                    $"\t\t r_exact match '{rule_string}' to '{r_second}' in \"{p_rule}\", rule number {r_num}");
+                                rule_number_match = r_num;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            String r_first = p_rule.Trim();
+                            
+                            if (rule_string.Equals(r_first))
+                            {
+                                Console.WriteLine(
+                                    $"\t o_exact match '{rule_string}' to '{r_first}' in \"{p_rule}\", rule number {r_num}");
+                                rule_number_match = r_num;
+                                break;
+                            }
+
+                                
+                        }
+
+                    
+                    }
+
+                    if (rule_number_match < 0)
+                    {
+                        Console.WriteLine($"\t No rule matches {rule_string}");
+                        return false;
+                    }
+                    else
+                    {
+                        new_list.AddLast(rule_number_match);
+                    }
+                }
+
+                Console.WriteLine($"\t Reduce {previous_terms} to {new_list.Count} terms");
+                terms = new_list;
+            }
+
+            Console.WriteLine($"\n\t Final reduction is to {Utility.LinkdedListToStringLine(terms)}");
             return false;
         }
+
 
         private static String[] rule_array;
         private static HashSet<String> valid_messages;
